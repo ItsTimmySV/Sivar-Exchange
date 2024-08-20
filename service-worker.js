f72471529e6e93b671e79052082edb82df194b0a
ItsTimmySV <josuegerardo00@gmail.com>
2024-08-20 14:26:14 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('v1').then(cache => {
+      return cache.addAll([
+        '/index.html',
+        '/styles.css',
+        '/img/logo.png',
+        '/manifest.json',
+        // Agrega aquí los demás archivos que quieres cachear
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
